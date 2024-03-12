3d74dabbe11e36dbd096748530159744dea6036a
d33bs <dave.bunten@cuanschutz.edu>
2024-03-12 12:48:18 -0600

    move sandbox content to tests

--- tests/test_build.py
+++ tests/test_build.py
@@ -5,8 +5,10 @@ Testing content builds and related aspects.
 import pathlib
 import subprocess
 
+from conftest import check_subproc_run_for_nonzero
 
-def test_links(build_jupyter_book: pathlib.Path):
+
+def test_links(build_jupyter_book: pathlib.Path) -> None:
     """
     Test links for the Jupyter Book build (html pages) using
     linkchecker python package.
@@ -24,14 +26,13 @@ def test_links(build_jupyter_book: pathlib.Path):
             # which would otherwise cause errors during link checks
             "--ignore-url",
             f"html/_static",
+            # used to check external-facing links
+            "--check-extern",
+            # used to avoid warnings triggering a non-zero return
+            "--no-warnings",
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
 
-    try:
-        # check that the build returns 0 (nothing failed)
-        assert result.returncode == 0
-    except Exception as exc:
-        # raise the exception with decoded output from linkchecker for readability
-        raise Exception(result.stdout.decode()) from exc
+    check_subproc_run_for_nonzero(completed_proc=result)
