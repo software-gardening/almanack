1f73d02821b2e90e33e671ddc5a850c7714da13d
d33bs <dave.bunten@cuanschutz.edu>
2024-05-28 14:45:47 -0600

    formatting and dynamic versioning

--- src/almanac/book.py
+++ src/almanac/book.py
@@ -1,12 +1,13 @@
 import pathlib
 
+
 # Example of displaying a specific chapter
-def display_chapter(chapter_name):
-    chapter_path = pathlib.Path(__file__).parent.parent / 'book' / chapter_name
-    
+def read(chapter_name):
+    chapter_path = pathlib.Path(__file__).parent.parent / "book" / chapter_name
+
     if not chapter_path.exists():
         print(f"Chapter {chapter_name} not found.")
         return
-    
+
     with chapter_path.open() as file:
         print(file.read())
