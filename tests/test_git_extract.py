766727c8876d63ccf4f250a2112278c4b220fac8
Will Davidson <willdavidson@willow.ucdenver.pvt>
2024-06-17 10:08:46 -0600

    Changing from absolute to relative path

--- tests/test_git_extract.py
+++ tests/test_git_extract.py
@@ -1,13 +1,15 @@
 import pathlib
 
 # Constants
-ENTROPY_PATH = "/home/willdavidson/Desktop/Almanack/almanac/tests/data/almanack/entropy"
-
+base_path = pathlib.Path(__file__).resolve().parent.parent.parent.parent
+ENTROPY_PATH = base_path / "/home/willdavidson/Desktop/Almanack/almanac/tests/data/almanack/entropy"
+ 
 
 def test_entropy_repos_deleted():
     """
     Test if the high_entropy and low_entropy repositories are deleted.
     """
+
     # Check if the directories are deleted
     assert not (pathlib.Path(ENTROPY_PATH) / "high_entropy").exists()
     assert not (pathlib.Path(ENTROPY_PATH) / "low_entropy").exists()
