33fae5b557c9fe4157a82f8fa643688a1ccecc82
Will Davidson <willdavidson@willow.ucdenver.pvt>
2024-06-24 14:33:07 -0600

    Creating proper test cases

--- tests/test_Loc.py
+++ tests/test_Loc.py
@@ -8,9 +8,7 @@ from almanack.LoC_tracker import calculate_loc_changes
 
 
 def test_calculate_loc_changes(repository_paths: dict[str, pathlib.Path]):
-    high_entropy_path = repository_paths["high_entropy"]
-    low_entropy_path = repository_paths["low_entropy"]
-
-    # Check that the changes are non-negative for each repository
-    assert calculate_loc_changes(high_entropy_path) > 0
-    assert calculate_loc_changes(low_entropy_path) > 0
+    # repository_paths.items() returns a dictionary
+    for _, repo_path in repository_paths.items():
+        # Check that the changes are non-negative for each repository
+        assert calculate_loc_changes(repo_path) > 0
