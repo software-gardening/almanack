d832b11e19e286ef8abc04b556175990e3b38ae3
Will Davidson <willdavidson@willow.ucdenver.pvt>
2024-06-24 14:21:08 -0600

    pre-commit

--- tests/test_Loc.py
+++ tests/test_Loc.py
@@ -4,7 +4,6 @@ Testing LoC_tracker functionality for calculating total lines changed in a given
 
 import pathlib
 
-import pytest
 
 from almanack.LoC_tracker import calculate_loc_changes
 
@@ -12,7 +11,7 @@ from almanack.LoC_tracker import calculate_loc_changes
 def test_calculate_loc_changes(repository_paths: dict[str, pathlib.Path]):
     high_entropy_path = repository_paths["high_entropy"]
     low_entropy_path = repository_paths["low_entropy"]
-        
+
     # Check that the changes are non-negative for each repository
     assert calculate_loc_changes(high_entropy_path) > 0
     assert calculate_loc_changes(low_entropy_path) > 0
