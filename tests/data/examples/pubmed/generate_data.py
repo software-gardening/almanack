634158d25db159aaf4abdd70da4015feec5cd1ed
d33bs <dave.bunten@cuanschutz.edu>
2024-07-23 10:17:19 -0600

    distinguish github pubmed data

--- tests/data/examples/pubmed/generate_data.py
+++ tests/data/examples/pubmed/generate_data.py
@@ -12,20 +12,14 @@ import re
 from typing import Any, Dict, List
 
 import pandas as pd
-import pytz
 import requests
 from Bio import Entrez
-from github import Auth, Github
 
 # pubmed requires we set an email which can be used through biopython
 # which is used for ratelimit warnings. We set this email from an
 # env var BIOPYTHON_PUBMED_EMAIL in the local system (set for example
 # by `export BIOPYTHON_PUBMED_EMAIL=email@address.edu`).
 Entrez.email = os.getenv("BIOPYTHON_PUBMED_EMAIL", "A.N.Other@example.com")
-
-github_client = Github(
-    auth=Auth.Token(os.environ.get("ALMANACK_ANALYSIS_GH_TOKEN")), per_page=100
-)
 
 
 def get_pubmed_articles_by_query(query: str, retmax: int = 10) -> List[Dict[str, Any]]:
@@ -153,37 +147,6 @@ def is_github_link_valid(link: str) -> bool:
         return False
 
 
-def try_to_detect_license(repo):
-    """
-    Tries to detect the license from GitHub API
-    """
-
-    try:
-        return repo.get_license().license.spdx_id
-    except:
-        return None
-
-
-def try_to_gather_commit_count(repo):
-    """
-    Tries to detect commit count of repo from GitHub API
-    """
-
-    try:
-        return len(list(repo.get_commits()))
-    except:
-        return 0
-
-
-def try_to_gather_most_recent_commit_date(repo):
-    """
-    Tries to detect most recent commit date of repo from GitHub API
-    """
-
-    try:
-        return repo.pushed_at.replace(tzinfo=pytz.UTC)
-    except:
-        return None
 
 
 df = pd.DataFrame(
@@ -223,61 +186,7 @@ df = df[df["IsValid"]].drop(columns="IsValid")
 
 print("invalid links removed!")
 
-df_github_data = pd.DataFrame(
-    # create a list of repo data records for a dataframe
-    [
-        {
-            "GitHub Name": repo.name,
-            "GitHub Repository ID": repo.id,
-            "GitHub Homepage": repo.homepage,
-            "github_link": link,
-            "GitHub Stars": repo.stargazers_count,
-            "GitHub Forks": repo.forks_count,
-            "GitHub Subscribers": repo.subscribers_count,
-            "GitHub Open Issues": repo.get_issues(state="open").totalCount,
-            "GitHub Contributors": repo.get_contributors().totalCount,
-            "GitHub License Type": try_to_detect_license(repo),
-            "GitHub Description": repo.description,
-            "GitHub Topics": repo.topics,
-            # gather org name if it exists
-            "GitHub Organization": (
-                repo.organization.login if repo.organization else None
-            ),
-            "GitHub Network Count": repo.network_count,
-            "GitHub Detected Languages": repo.get_languages(),
-            "Date Created": repo.created_at.replace(tzinfo=pytz.UTC),
-            "Date Most Recent Commit": try_to_gather_most_recent_commit_date(repo),
-            # placeholders for later datetime calculations
-            "Duration Created to Most Recent Commit": "",
-            "Duration Created to Now": "",
-            "Duration Most Recent Commit to Now": "",
-            "Repository Size (KB)": repo.size,
-            "GitHub Repo Archived": repo.archived,
-        }
-        # make a request for github repo data with pygithub
-        for link, repo in [
-            (
-                github_link,
-                github_client.get_repo(github_link.replace("https://github.com/", "")),
-            )
-            for github_link in df["github_link"].tolist()
-        ]
-    ]
-)
-
-print("GitHub details gathered!")
-
-df_final = pd.merge(
-    left=df,
-    right=df_github_data,
-    how="left",
-    left_on="github_link",
-    right_on="github_link",
-)
-
-print("Data merged!")
-
 # export data to parquet
-df_final.to_parquet(
+df.to_parquet(
     "tests/data/examples/pubmed/pubmed_github_links.parquet", compression="zstd"
 )
