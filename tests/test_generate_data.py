bb4b70aa727f6d6d75644001b4765c061aa6eeaf
Will Davidson <willdavidson@willow.ucdenver.pvt>
2024-08-07 10:53:03 -0600

    Test cases

--- tests/test_generate_data.py
+++ tests/test_generate_data.py
@@ -7,18 +7,19 @@ import pathlib
 from almanack.processing.generate_data import generate_whole_repo_data
 
 
-def test_process_repo_entropy(repository_paths: dict[str, pathlib.Path]) -> None:
+def test_generate_whole_repo_data(repository_paths: dict[str, pathlib.Path]) -> None:
     """
-    Testing process_repo_entropy produces the expected JSON output for given repositories.
+    Testing generate_whole_repo_data produces the expected output for given repositories.
     """
     for label, repo_path in repository_paths.items():
-        # Call the function directly and get the dictionary
-        entropy_data = generate_whole_repo_data(str(repo_path))
+        # Call the function
+        data = generate_whole_repo_data(str(repo_path))
 
-        # Check that the returned data is a dictionary
-        assert isinstance(entropy_data, dict)
+        # Check that data is not None and it's a dictionary
+        assert data is not None
+        assert isinstance(data, dict)
 
-        # Check for expected keys in the output
+        # Check for expected keys
         expected_keys = [
             "repo_path",
             "total_normalized_entropy",
@@ -27,6 +28,7 @@ def test_process_repo_entropy(repository_paths: dict[str, pathlib.Path]) -> None
             "time_range_of_commits",
             "file_level_entropy",
         ]
+        assert all(key in data for key in expected_keys)
 
-        # Check if all expected keys are present in the entropy_data
-        assert all(key in entropy_data for key in expected_keys)
+        # Check that repo_path in the output is the same as the input
+        assert data["repo_path"] == str(repo_path)
--- tests/test_processing_repositories.py
+++ tests/test_processing_repositories.py
@@ -5,7 +5,6 @@ Testing report.py functionality
 import json
 import pathlib
 
-
 from almanack.processing.processing_repositories import process_repo_entropy
 
 
