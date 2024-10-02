ad5b6c7ccf8aedfb2812f2b54cf28c88423987a0
d33bs <dave.bunten@cuanschutz.edu>
2024-10-02 14:13:15 -0600

    fix tests for build

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,21 +9,8 @@ import subprocess
 
 import pytest
 
+from .utils import check_subproc_run_for_nonzero
 from tests.data.almanack.repo_setup.create_repo import create_repositories
-
-
-def check_subproc_run_for_nonzero(completed_proc: subprocess.CompletedProcess) -> None:
-    """
-    Checks subprocess.CompletedProcess for errors and displays stdout
-    in a legible way through pytest.
-    """
-
-    try:
-        # check that the build returns 0 (nothing failed)
-        assert completed_proc.returncode == 0
-    except Exception as exc:
-        # raise the exception with decoded output from linkchecker for readability
-        raise Exception(completed_proc.stdout.decode()) from exc
 
 
 @pytest.fixture()
