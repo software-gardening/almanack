9d13a6ba98d17333cc7b2f37e3f58354f82f72f4
d33bs <dave.bunten@cuanschutz.edu>
2024-12-03 07:59:25 -0700

    increase backoff

--- src/almanack/metrics/data.py
+++ src/almanack/metrics/data.py
@@ -685,7 +685,7 @@ def get_api_data(
         params = {}
 
     retries = 30  # Number of attempts for rate limit errors
-    backoff = 15  # Seconds to wait between retries
+    backoff = 25  # Seconds to wait between retries
 
     for attempt in range(retries):
         try:
